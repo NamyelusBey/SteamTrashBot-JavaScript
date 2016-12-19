440ea765d9eb0417805cd9f18435aa50fa463c5a
DrGumik <jakubtenk@seznam.cz>
2016-12-19 14:49:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,18 +14,24 @@ var SteamID = require("steamid");
 var SteamTrade = require("steam-trade");
 var SteamTrade = new SteamTrade();
 
+////////////////////// CHANGE ONLY YourAccountName, YourPassword and STEAMID IN WITHDRAW
+
 client.logOn({
-   "accountName": "YourAccountLoginName",
+   "accountName": "YourAccountName",
    "password": "YourPassword"
  });
 
  client.on("loggedOn", function(details){
    console.log("Logged On To Steam Server With The ID Of " + client.steamID.getSteam3RenderedID());
    client.setPersona(SteamUser.Steam.EPersonaState.Online);
-   client.gamesPlayed(440);
+   client.gamesPlayed(730);
  });
-
+console.log("------------------------------------------------------------------")
+console.log("CHANGE ONLY YourAccountName, YourPassword and STEAMID IN WITHDRAW")
 console.log("##  SteamBot Made by GumikCZ  ##")
+console.log("Have a problem?? My youtube channel: https://www.youtube.com/channel/UCd2ujTlK79FsUfF1aEJnh3w")
+console.log("My GitHub with Projects: https://github.com/DrGumik")
+console.log("------------------------------------------------------------------")
 
 client.on("webSession", function(steamID, cookies){
 community.setCookies(cookies);
@@ -37,32 +43,32 @@ SteamTrade.setCookie(cookie);
  client.on("error", function(error){
    console.log("Error");
 });
-///////////////////////////////////////////////////////////////////////////////
+
 friends.on("friendMsg", function(user, msg, type){
  if(type == Steam.EChatEntryType.ChatMsg){
-  if(msg == "/commands"){
-   friends.sendMessage(user,"The Commands Are: /joke");
+  if(msg == "Hello"){
+   friends.sendMessage(user,"Hello!Send me trade invite.");
    }
  }
 })
-
+/////////////////////////////////////////////////////// WITHDRAW SYSTEM
 SteamTrade.on("chatMsg", function(msg)  {
-if (msg == "!item" && "76561198065953943")
-console.log("Giving Items");
-SteamTrade.loadInventory(730,1, function(items){
-SteamTrade.addItems();
+if (msg == "!itemcsgo" && "Your STEAMID") //// You can find steamID on steamrep.com
+console.log("Giving Items from CSGO!");
+SteamTrade.loadInventory(730,2, function(items){
+SteamTrade.addItems(items);
    })
+ })
+})
+/////////////////////////////////////////////////////////////////
+SteamTrade.on("ready", function()  {
+SteamTrade.ready(function()  {
+  SteamTrade.confirm();
  })
 })
 
 client.on("tradeStarted", function(steamID){
-  SteamTrade.open(steamID)
-})
-
-SteamTrade.on("ready", function()  {
-  SteamTrade.ready(function()  {
-    SteamTrade.confirm();
- })
+  SteamTrade.open(steamID);
 })
 
 client.on("tradeRequest", function(steamID, respond){
@@ -77,3 +83,6 @@ SteamTrade.on('end', function(status, getItems) {
     });
   }
 });
+///////////////////////////////////////////////////////////////////////////////
+////My GitHub: https://github.com/DrGumik
+///////////////////////////////////////////////////////////////////////////////
